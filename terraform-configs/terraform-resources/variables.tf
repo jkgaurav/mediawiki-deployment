2fb8fbda9b17a1746a3d569d27a245f07492e3d8
Gaurav Kumar <jk.gaurav@gmail.com>
2024-06-08 16:10:06 +0530

    create templates for azure resources

--- terraform-configs/terraform-resources/variables.tf
+++ terraform-configs/terraform-resources/variables.tf
@@ -11,5 +11,11 @@ variable "resource_group_location" {
 
 ### Storage Account
 variable "storage_account_name" {
-  
+  description = "Name of SA"
+  default = "mediawikideploymentsa01"
+}
+
+variable "storage_account_container_name" {
+  description = "Name of Container in Storage Account"
+  default = "tfstate"
 }
