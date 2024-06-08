277d51d6008940ea06923480a8ffab33858d2ae8
Gaurav Kumar <jk.gaurav@gmail.com>
2024-06-08 17:32:06 +0530

    add acr

--- terraform-configs/terraform-resources/outputs.tf
+++ terraform-configs/terraform-resources/outputs.tf
@@ -2,3 +2,16 @@ output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
   sensitive = true
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
