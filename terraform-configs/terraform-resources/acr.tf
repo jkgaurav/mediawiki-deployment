d484f26c678b15eb0b625ce594f1b50793ccdd26
Gaurav Kumar <jk.gaurav@gmail.com>
2024-06-09 16:39:45 +0530

    deploy several changes

--- terraform-configs/terraform-resources/acr.tf
+++ terraform-configs/terraform-resources/acr.tf
@@ -5,3 +5,9 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope = azurerm_container_registry.acr.id
+}
